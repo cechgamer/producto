9ca50e9baa065cb14ac522166862c7cb834ed969
cech.castellon <47920140+cechgamer@users.noreply.github.com>
2019-03-09 10:14:37 -0400

    primer commit aux

---
+++ ProductosClientes/ProductosClientes/Models/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductosClientes.Models
+{
+    public class DataContext::DBContext
+    {
+    }
+}
---
+++ ProductosClientes/ProductosClientes/Models/Productos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductosClientes.Models
+{
+    public class Productos
+    {
+        int ProductosID;
+        string Nombre;
+        double Precio;
+
+
+
+    }
+}
